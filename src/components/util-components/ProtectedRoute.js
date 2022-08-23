2bac191f11863bc0ca9e371b3d7534bce2458d70
AhmedZohny208 <asfmz754@gmail.com>
2022-08-23 13:14:16 +0200

    Fixed protected routes

--- src/components/util-components/ProtectedRoute.js
+++ src/components/util-components/ProtectedRoute.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import jwtDecode from 'jwt-decode';
 import { AUTH_PREFIX_PATH } from 'configs/AppConfig'
 import { getProfileData } from "redux/actions/Profile";
 
@@ -13,13 +14,34 @@ export default function ProtectedRoute({ component: Component, ...rest }) {
     dispatch(getProfileData())
   }, [dispatch])
 
+  function isAuth() {
+    const token = localStorage.getItem('HaressOwnerjwtToken');
+    try {
+      const { exp } = jwtDecode(token);
+      if (Date.now() >= exp * 1000) {
+        return false;
+      }
+    } catch (err) {
+      return false;
+    }
+    return true;
+  }
+  let isAuthValid = isAuth()
+
   return (
     <>
       {loading === false && (
         <Route 
           {...rest}
           render={props => {
-            if (isAuthenticated === false) {
+            if (!localStorage.getItem('HaressOwnerjwtToken')) {
+              return <Redirect to={`${AUTH_PREFIX_PATH}/login`} />
+            }
+            if (!isAuthValid) {
+              return <Redirect to={`${AUTH_PREFIX_PATH}/login`} />
+            }
+
+            if (!isAuthValid && isAuthenticated === false) {
               return <Redirect to={`${AUTH_PREFIX_PATH}/login`} />
             }
 
